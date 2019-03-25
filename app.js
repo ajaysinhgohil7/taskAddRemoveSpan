3d75d278d78aef051340f509f1155a3be773ce6c
admin2 <ajay.gohil@rapidops.com>
2019-03-25 18:59:22 +0530

    Added first child from JS file Removed from html

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-let inputCount = 1;
+let inputCount = 0;
 
 let mainBody = document.getElementById('main');
+
+let spanFirst = document.createElement('span');
+spanFirst.innerHTML = '<input id="inputBox'+ inputCount +'" />'+
+                 '<button id="cancelBtn' + inputCount +'" onclick="addInputAndButton()"> Add </button>';
+spanFirst.id = "span"+inputCount;
+mainBody.prepend(spanFirst);
+
 
 let removeInputAndButton = function(inputCount){
     console.log("inside remove " + inputCount);
@@ -13,12 +20,16 @@ let removeInputAndButton = function(inputCount){
     
     document.getElementById(rInput).remove();
     document.getElementById(rButton).remove();
+
     inputCount = inputCount - 1;
 };
 
 
+
+
 let addInputAndButton = function (){
     
+    inputCount = inputCount + 1;
     // var cancelBtn = document.createElement("BUTTON");
     
     // cancelBtn.innerHTML="cancelBtn";
@@ -30,17 +41,16 @@ let addInputAndButton = function (){
     
     // document.body.prepend(cancelBtn);
     
-    // var textBox = document.createElement("INPUT");
+    // let textBox = document.createElement("INPUT");
     // document.body.prepend(textBox);
     // textBox.id="inputBox"+inputCount;
     
     
-    var span = document.createElement('span');
+    let span = document.createElement('span');
     span.innerHTML = '<input id="inputBox'+ inputCount +'" />'+
                      '<button id="cancelBtn' + inputCount +'" onclick="removeInputAndButton('+ inputCount +')"> Remove </button>';
     span.id = "span"+inputCount;
     mainBody.prepend(span);
     
-    inputCount = inputCount + 1;
 };
  
