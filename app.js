053bbac740c13b0f932105a6109eacbb3d1d50c2
admin2 <ajay.gohil@rapidops.com>
2019-03-27 16:23:55 +0530

    merged With Origin/reAddingDeletedSpans

--- app.js
+++ app.js
@@ -13,18 +13,17 @@ let deletedQueueSpanIDs = [];
 
 let spanFirst = document.createElement('span');
 spanFirst.innerHTML = '<input id="inputBox'+ inputCount +'" />'+
-                 '<button id="cancelBtn' + inputCount +'" onclick="addInputAndButton()"> Add </button>';
+                        '<button id="cancelBtn' + inputCount +'" onclick="addInputAndButton()"> Add </button>';
 spanFirst.id = "span"+inputCount;
 mainBody.prepend(spanFirst);
 
 
 removeInputAndButton = function(e){
-    // let tempVar = inputCount;
+
     console.log(e);
     console.log(e.target.parentNode.id);
     let parentNodeSpan = e.target.parentNode.id;
 
-    
     document.getElementById(parentNodeSpan).remove();
   
     inputCount = inputCount - 1;
@@ -37,6 +36,7 @@ removeInputAndButton = function(e){
 
 
 let getNumberOutOfStringArray = function () {
+    deletedQueueSpanIDs = [];
     for (let i = 0; i < deletedQueue.length; i++) {
         let element = deletedQueue[i];
         deletedQueueSpanIDs.push(parseInt(element.match(/\d+/g)[0]));
@@ -49,30 +49,39 @@ let getNumberOutOfStringArray = function () {
 addInputAndButton = function (){
         let tempdeletedQueueSpanIDs = getNumberOutOfStringArray();
         if(tempdeletedQueueSpanIDs.length > 0){
-            alert("max = " + Math.max(...tempdeletedQueueSpanIDs));
-            alert("min = " + Math.min(...tempdeletedQueueSpanIDs));
-        }
-
-        let avalaibleID = checkAvailability('span'+(inputCount+1));
-        if(avalaibleID !== null){
-            alert('found');
-
-            deletedQueueItem = deletedQueue[avalaibleID];
-            deletedQueueItemID = deletedQueueItem.match(/\d+/g).map(Number)[0];
-
-            alert(deletedQueueItemID);
 
             inputCount = inputCount + 1;
+            let minDeleted = Math.min(...tempdeletedQueueSpanIDs);
             let span = document.createElement('span');
-            span.id = "span" + deletedQueueItemID;
-            span.innerHTML = '<input id="inputBox'+ deletedQueueItemID +'" />'+
-                                '<button id="cancelBtn' + deletedQueueItemID +'" onclick="removeInputAndButton(event)"> Remove </button><br>';
+            span.id = "span" + minDeleted;
+            span.innerHTML = '<input id="inputBox'+ minDeleted +'" />'+
+                                '<button id="cancelBtn' + minDeleted +'" onclick="removeInputAndButton(event)"> Remove </button><br>';
             mainBody.prepend(span);
 
 
-            deletedQueue.splice(avalaibleID, 1);
+            deletedQueue.splice(deletedQueue.indexOf("span"+minDeleted), 1);
+            deletedQueueSpanIDs.splice(deletedQueueSpanIDs.indexOf(minDeleted), 1);
+            console.log("ADDDDDDDDDDDD"); 
+            console.log("deletedQueue"); 
+            console.log(deletedQueue); 
+            console.log("deletedQueueSpanIDs"); 
+            console.log(deletedQueueSpanIDs); 
 
         }else{
+
+            console.log("mainBody.childrens");
+            let childrens = mainBody.children;
+            console.log(childrens);
+            let childrensSpan = 0;
+            let spanArray = [];
+            for (let i = 0; i < childrens.length; i++) {
+                if(childrens[i].tagName == "SPAN"){
+                    childrensSpan += 1;
+                }
+            }
+
+            console.log(childrensSpan);
+            
             inputCount = inputCount + 1;
             let span = document.createElement('span');
             span.id = "span"+inputCount;
@@ -80,47 +89,43 @@ addInputAndButton = function (){
                                 '<button id="cancelBtn' + inputCount +'" onclick="removeInputAndButton(event)"> Remove </button><br>';
             mainBody.prepend(span);
 
-            // addSpan();
         }
 };
 
-// addSpan = function (newID) {
-    
-   
-// };
-
-
 showAllInputValues = function () {
 
-    if (document.body.contains(document.getElementById("spanTable1"))) {
-        document.getElementById("spanTable1").remove();
+    if (document.body.contains(document.getElementById("tableInputs"))) {
+        document.getElementById("tableInputs").remove();
     }
 
     let table = document.createElement('TABLE');
     table.id = "tableInputs";
+    table.style.border = "2px solid black";
 
-    let spanTable = document.createElement('span');
-    spanTable.innerHTML = '<table id="spanTable1" style="border: 2px solid black;"></table>';
-    // spanTable.id = "spanTable1";
-    mainBody.append(spanTable);
+    mainBody.append(table);
 
-    for(let i=0; i <= inputCount; i++){
-        if(document.getElementById('inputBox'+i) != null){
-            if( document.getElementById('inputBox'+i).value != "" 
-                    && document.getElementById('inputBox'+i).value != null
-                    && document.getElementById('inputBox'+i).value != undefined ){
+    for(let k=0; k < mainBody.children.length; k++){
+        if(mainBody.children[k].tagName == 'SPAN'){
+            let spanElement = mainBody.children[k];
+            let inputBoxValue = spanElement.firstChild.value;
 
-                        let tr =  document.createElement('tr');
-            
-                        let td = document.createElement('td');
-            
-                        let data = document.createTextNode(document.getElementById('inputBox'+i).value);
-            
-                        td.appendChild(data);
-                        tr.appendChild(td);
-                        document.getElementById('spanTable1').appendChild(tr);
-                        // table.appendChild(tr);
+            if(spanElement != null){
+                
+                if( inputBoxValue != "" && inputBoxValue != null && inputBoxValue != undefined ){
+    
+                            let tr =  document.createElement('tr');
+                
+                            let td = document.createElement('td');
+                
+                            let data = document.createTextNode(inputBoxValue);
+                
+                            td.appendChild(data);
+                            tr.appendChild(td);
+                            document.getElementById('tableInputs').appendChild(tr);
+                            // table.appendChild(tr);
+                }
             }
+
         }
     }
 };
@@ -129,14 +134,6 @@ addToDeletedQueue = function (parentNodeSpan) {
     
     deletedQueue.push(parentNodeSpan);
 
-    console.log("deletedQueue");
-    console.log(deletedQueue);
-
-};
-
-deleteFromDeletedQueue = function (parentNodeSpan) {
-    
-    deletedQueue.push(parentNodeSpan);
     console.log("deletedQueue");
     console.log(deletedQueue);
 
